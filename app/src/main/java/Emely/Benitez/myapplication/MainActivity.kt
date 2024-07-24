ef35236c10a7d9eb7715f3f5e841a53839f2da65
Emely <20230621@ricaldone.edu.sv>
2024-07-24 15:49:18 -0300

    Cambios

--- app/src/main/java/Emely/Benitez/myapplication/MainActivity.kt
+++ app/src/main/java/Emely/Benitez/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import Emely.Benitez.myapplication.databinding.ActivityMain2Binding
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -33,4 +35,26 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
         }
+
+    val lbNombrePacientes = findViewById<TextView>(R.id.lbNombrePaciente)
+    val lbApellidoPacientes = findViewById<TextView>(R.id.lbApellidoPaciente)
+    val lbEdad = findViewById<TextView>(R.id.lbEdadPaciente)
+    val lbEnfermedad = findViewById<TextView>(R.id.lbEnfermedad)
+    val lbNumCama = findViewById<TextView>(R.id.lbNumerodeCama)
+    val lbNumHabitacion = findViewById<TextView>(R.id.lbNumerodeHabitacion)
+    val lbHoradeMedicacion = findViewById<TextView>(R.id.lbHoradeMedicacion)
+    val lbMedicamentosAsignados = findViewById<TextView>(R.id.lbMedicamentosAsignados)
+    val lbFechaIngreso = findViewById<TextView>(R.id.lbFechaIngreso)
+
+
+    lbNombrePacientes.text = intent.getStringExtra("Nombre")
+    lbApellidosPaciente.text = intent.getStringExtra("Apellido")
+    lbEdad.text = intent.getIntExtra("Edad",0).toString()
+    lbEnfermedad.text = intent.getStringExtra("Enfermedad")
+    lbNumHabitacion.text = intent.getIntExtra("Numero de Habitacion",0).toString()
+    lbNumCama.text = intent.getIntExtra("Numero de Cama",0).toString()
+    lbMedicamentos.text = intent.getStringExtra("Medicamentos")
+    lbFechaIngreso.text = intent.getStringExtra("Fecha Ingreso")
+    lbHoraMedicamentos.text = intent.getStringExtra("Hora de Aplicacion")
+
     }
